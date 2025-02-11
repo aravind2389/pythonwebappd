a429f4b64a80c51c3b3677d54c7057027c35653e
aravindk <aravindkundurthi98@gmail.com>
2025-02-11 14:45:19 +0530

    webapp.py code updated

--- webapp.py
+++ webapp.py
@@ -1,9 +1,19 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return "Response reached"
-
-if __name__ == '__main__':
-    app.run(debug=True)
+import requests
+ 
+print("\t\tWelcome to the Weather Forecaster!\n\n")
+print("Just Enter the City you want the weather report for and click on the button! It's that simple!\n\n")
+ 
+city_name = input("Enter the name of the City : ")
+print("\n\n")
+ 
+# Function to Generate Report
+def Gen_report(C):
+    url = 'https://wttr.in/{}'.format(C)
+    try:
+        data = requests.get(url)
+        T = data.text
+    except:
+        T = "Error Occurred"
+    print(T)
+     
+Gen_report(city_name)
