1a8ce234e968d4bc63d482a782cee0575a4448d8
aravindk <aravindkundurthi98@gmail.com>
2025-02-21 11:41:58 +0530

    deleted unused files

--- test.py
+++
@@ -1,9 +0,0 @@
-s = "geeksforGeeks"
-
-# Trying to change the first character raises an error
-# s[0] = 'I'  # Uncommenting this line will cause a TypeError
-
-# Instead, create a new string
-s = "G" + s[0:]
-print(s)
-
